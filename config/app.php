bf30a371d8d04c32bc419b996756bf22711dd3a8
ReneGarBer <67211759+ReneGarBer@users.noreply.github.com>
2022-05-28 10:47:17 -0500

    comment key,122 , change url,55, debug,42

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://blogproyectofinal.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    //'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
 
